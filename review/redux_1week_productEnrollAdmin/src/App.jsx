81650f5219a5d6667248784d7d29877fe3b1400f
kokoball <kbp4154@naver.com>
2022-02-04 17:01:16 +0900

    :bug: (Fix: bug)

--- review/redux_1week_productEnrollAdmin/src/App.jsx
+++ review/redux_1week_productEnrollAdmin/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/product" element={<ProductionInformationPage />} />
             <Route path="/option" element={<OptionTablePage />} />
-            <Route path="/item" element={<ItemInformationTablePage />} />
+            <Route path="/item" element={<ItemInformationTablePage />} />s
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
